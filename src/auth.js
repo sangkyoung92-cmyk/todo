26845a1902cef1e64aafd372b159366ce1b84a78
Sunya <sangkyoung92@gmail.com>
2026-07-15 10:56:04 +0900

    Show login landing immediately on sign out

--- src/auth.js
+++ src/auth.js
@@ -4,6 +4,9 @@ import {
   signInWithRedirect,
   signOut,
   onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
 } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
 import { auth } from './firebase-config.js';
 
@@ -15,6 +18,18 @@ export function signIn() {
 
 export function signInRedirect() {
   return signInWithRedirect(auth, provider);
+}
+
+export function signUpWithEmail(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function signInWithEmail(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function sendPasswordReset(email) {
+  return sendPasswordResetEmail(auth, email);
 }
 
 export function signOutUser() {
